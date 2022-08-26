c5bf43eb07102c5649f1ccfebe217ff9e7ee8241
neys1209 <90494801+neys1209@users.noreply.github.com>
2022-08-26 13:23:26 +0900

    first file

---
+++ GitHubTest/Assets/Script/Cube.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    Vector3 pos = Vector3.zero;
+    float num = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        num += Time.deltaTime;
+        transform.position = pos + new Vector3(0,Mathf.Sin(num),0);
+    }
+}
